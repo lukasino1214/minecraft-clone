b013a6c92a03660f88b98255a49591690cfa92b4
lukasino1214 <lukasino1214@gmail.com>
2021-12-15 16:50:16 +0100

    Init

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../deps/imgui/imgui.h"
+#include "../deps/imgui/imgui_impl_opengl3.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <spdlog/spdlog.h>
+#include <stb_image.h>
+#include <entt/entt.hpp>
+#include <yaml-cpp/yaml.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ xmake.lua
@@ -0,0 +1,20 @@
+set_allowedarchs("x64")
+set_languages("c99", "c++17")
+
+packages = {"glfw", "glad", "glm", "spdlog", "yaml-cpp", "entt", "stb"}
+
+add_requires(packages)
+
+target("deps")
+    set_kind("static")
+    add_files("deps/*/*.cpp")
+    add_headerfiles("deps/*/*.h")
+    add_packages("glfw", "glad")
+
+target("EngineEditor")
+    set_kind("binary")
+    add_files("src/*.cpp", "src/*/*.cpp")
+    add_headerfiles("src/*.h", "src/*/*.h")
+    set_optimize("fastest")
+    add_deps("deps")
+    add_packages(packages)
